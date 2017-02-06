72f1beba35216f47b95c9b2bcf1030e85ed36e10
Dale Avery <davery@us.ibm.com>
2017-02-06 16:26:43 -0500

    First attempt at a UnaryInterceptor for statsd metrics

---
+++ core/comm/metrics.go
@@ -0,0 +1,37 @@
+/*
+Copyright IBM Corp. 2016 All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+		 http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package comm
+
+import (
+	"github.com/cactus/go-statsd-client/statsd"
+	"github.com/hyperledger/fabric/vendor/google.golang.org/grpc"
+	"golang.org/x/net/context"
+)
+
+// UnaryMetricsInterceptor intercepts Unary traffic and sends statsd metrics.
+func UnaryMetricsInterceptor(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (resp interface{}, err error) {
+	// Print out the grpc traffic info.  Anything will do
+	commLogger.Debug("UnaryMetricsInterceptor called")
+
+	// Send a metric to statsd
+	commLogger.Debug("Sending statsd metric")
+	client, _ := statsd.NewClient("127.0.0.1:8125", "unary_interceptor")
+	client.Inc("interceptions", 1, 1.0)
+
+	// Call handler to complete the RPC reqest, the same as next() in express
+	return handler(ctx, req)
+}
